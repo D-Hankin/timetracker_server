05351bffba303a143b2b0bab890cb89d8c837771
David Hankin <d_hankin@outlook.com>
2024-05-08 09:26:45 +0200

    return role with token

--- src/main/java/org/timetracker_server/models/TokenResponse.java
+++ src/main/java/org/timetracker_server/models/TokenResponse.java
@@ -3,13 +3,15 @@ package org.timetracker_server.models;
 public class TokenResponse {
     private String token;
     private String expiresIn;
+    private String role;
 
     public TokenResponse() {
     }
 
-    public TokenResponse(String token, String expiresIn) {
+    public TokenResponse(String token, String expiresIn, String role) {
         this.token = token;
         this.expiresIn = expiresIn;
+        this.role = role;
     }
 
     public String getToken() {
@@ -26,6 +28,14 @@ public class TokenResponse {
 
     public void setExpiresIn(String expiresIn) {
         this.expiresIn = expiresIn;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
 
